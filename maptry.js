8a26aeb99eea75717e5820f968e7950f9b2e8c73
iamgrevy <kh.noza2mail.ru>
2021-06-25 12:10:17 +0300

    add

--- maptry.js
+++ maptry.js
@@ -1,47 +1,60 @@
 fetch("mj.json")
-.then(function (response) {
+  .then(function (response) {
     return response.json();
-
-})
-.then(function (jsonData) {
+  })
+  .then(function (jsonData) {
     console.log(jsonData);
-  console.log("doin smth else");
+    console.log("doin smth else");
 
-  var mymap = L.map('mapid', {
-    crs: L.CRS.Simple
-});
-var bounds = [[0,0], [10000,10000]];
-var image = L.imageOverlay('Untitled-1-01.png', bounds).addTo(map);
-map.fitBounds(bounds)
+    var map = L.map("mapid", {
+      crs: L.CRS.Simple,
+    });
+    var bounds = [
+      [0, 0],
+      [10000, 10000],
+    ];
+    // var image = L.imageOverlay("Untitled-1-01.png", bounds).addTo(map);
+    map.fitBounds(bounds);
 
-// var firstGuyPath = jsonData1.filter(function(jsonData1) 
-// { return jsonData1['tag-local-identifier'] === 'D 51153'; });
-// console.log(firstGuyPath);
+    // var firstGuyPath = jsonData1.filter(function(jsonData1)
+    // { return jsonData1['tag-local-identifier'] === 'D 51153'; });
+    // console.log(firstGuyPath);
 
-   var michaeljacksonLocation = jsonData.map(function(jsonData) 
-   { // каждый элемент массива превращается в [lat, lng]
-    return [jsonData["location-lat"], jsonData["location-long"]];
-  });
-  console.log(michaeljacksonLocation);
+    var michaeljacksonLocation = jsonData.map(function (jsonData) {
+      // каждый элемент массива превращается в [lat, lng]
+      return [jsonData["location-lat"], jsonData["location-long"]];
+    });
+    // console.log(michaeljacksonLocation);
 
-//   
-
-var polyline = L.polyline(michaeljacksonLocation, { color: "blue" }).addTo(mymap);
-mymap.fitBounds(polyline.getBounds());
+    var polyline = L.polyline(michaeljacksonLocation, { color: "blue" }).addTo(
+      map
+    );
+    map.fitBounds(polyline.getBounds());
     polyline.bindPopup("MJ");
-
 
     // для каждого элемента массива jsonData
     jsonData.forEach(function (jsonData) {
       // создаём кружок с координатами, взятыми из элемента массива (переменная stork)
-      var circle = L.circle([jsonData["location-lat"], jsonData["location-long"]], {
-        radius: 10000,
-      });
+      var circle = L.circle(
+        [jsonData["location-lat"], jsonData["location-long"]],
+        {
+          radius: 1,
+        }
+      );
       // привязываем к кружку поп-ап с данными, взятыми из элемента массива (переменная stork)
-      var note = "MJ" + jsonData.city.toUpperCase() + jsonData.time.toUpperCase();
-      circle.bindPopup(note);
+      var note =
+        "MJ" + jsonData.city.toUpperCase() + jsonData.time.toUpperCase();
+      var popup = document.createElement("div");
+      popup.classList.add("my-popup");
+      var popupText = document.createElement("div");
+      popupText.innerHTML = note;
+      var popupImg = document.createElement("img");
+      popupImg.src = jsonData.imgSrc;
+      popupImg.style.width = "100%";
+      popup.appendChild(popupText);
+      popup.appendChild(popupImg);
+      circle.bindPopup(popup, { maxWidth: 250 });
       // добавляем кружок на карту
-      circle.addTo(mymap);
+      circle.addTo(map);
     });
-
-    });
+  });
